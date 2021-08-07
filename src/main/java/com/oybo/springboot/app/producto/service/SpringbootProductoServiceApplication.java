edecde78fd4f8eb10bf6fcef79e306ee52abe57f
Oscar Bautista Windows <oscar.bautista@hotmail.com>
2021-08-07 18:47:45 -0500

    Se inicia proyecto

---
+++ src/main/java/com/oybo/springboot/app/producto/service/SpringbootProductoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oybo.springboot.app.producto.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProductoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProductoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oybo/springboot/app/producto/service/SpringbootProductoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oybo.springboot.app.producto.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProductoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
